87c3631449b53d7fd1e6b7eca992424f55b0166c
mac <dimasobchenko.94@gmail.com>
2021-02-02 10:20:30 +0200

    fetch

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,19 +1,24 @@
- const  email = document.querySelector('#login'),
-        password = document.querySelector('#password'),
-        signIn = document.querySelector('#check');
+const email = document.querySelector('#login'),
+      password = document.querySelector('#password'),
+      signIn = document.querySelector('#check');
 
 class Fetch {
     constructor(){
-            this.url = {
-                login: 'https://ajax.test-danit.com/api/v2/cards/login',
-                cards: 'https://ajax.test-danit.com/api/v2/cards'
-            }
+        this.url = {
+            login: 'https://ajax.test-danit.com/api/v2/cards/login',
+            cards: 'https://ajax.test-danit.com/api/v2/cards'
         }
-        saveToken(token){
-        localStorage.setItem('tokenData', JSON.stringify(token))
-        }
+    }
 
-    async changeFetch(id='',value){
+    // Додавання токена в локал
+
+    saveToken(token){
+        localStorage.setItem('tokenData', JSON.stringify(token))
+    }
+
+    // заміна карти
+
+    async putFetch(id='',value){
         const response = await fetch(`${this.url.cards}/${id}`,{
             method: "PUT",
             headers: {
@@ -26,68 +31,77 @@ class Fetch {
         return data;
     }
 
+// Отримання токена
+
     async getToken (email, password){
-            const response = await fetch(this.url.login,{
-                method: "POST",
-                headers: {
-                    "Content-type":"application/json",
-                },
-                body: JSON.stringify({
-                    'email': email,
-                    'password': password,
-                })
-            });
-            if(response.status === 200){
-                const data = await response.text();
-                console.log(data);
-                this.saveToken(data);
-                return data
-            }else return false
-        }
+        const response = await fetch(this.url.login,{
+            method: "POST",
+            headers: {
+                "Content-type":"application/json",
+            },
+            body: JSON.stringify({
+                'email': email,
+                'password': password,
+            })
+        });
+        if(response.status === 200){
+            const data = await response.text();
+            console.log(data);
+            this.saveToken(data);
+            return data
+        }else return false
+    }
 
-     async createFetch (value){
-            const response = await fetch(this.url.cards,{
-                method: 'POST',
-                headers: {
-                    "Content-type":"application/json",
-                    'Authorization': ` Bearer ${localStorage.tokenData.split('\"')[1]}`
-                },
-                body: JSON.stringify(value)
-            });
-             const data = await response.text();
-             console.log(data);
-             return data;
-     }
+// створення карти
 
-     async takeFetch(id=''){
-            const response = await fetch(`${this.url.cards}/${id}`,{
-                method: "GET",
-                headers: {
-                    "Content-type":"application/json",
-                    'Authorization': ` Bearer ${localStorage.tokenData.split('\"')[1]}`
-                }
-            });
-         const data = await response.json();
-         console.log(data);
-         return data
-     }
+    async postFetch (value){
+        const response = await fetch(this.url.cards,{
+            method: 'POST',
+            headers: {
+                "Content-type":"application/json",
+                'Authorization': ` Bearer ${localStorage.tokenData.split('\"')[1]}`
+            },
+            body: JSON.stringify(value)
+        });
+        const data = await response.text();
+        console.log(data);
+        return data;
+    }
+// Отримання всіх карт
+
+    async getFetch(id=''){
+        const response = await fetch(`${this.url.cards}/${id}`,{
+            method: "GET",
+            headers: {
+                "Content-type":"application/json",
+                'Authorization': `Bearer ${localStorage.tokenData.split('\"')[1]}`
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+        return data
+    }
+
+ // Видалення токена по ид
 
     async deleteFetch (id){
-            const response = await fetch(`https://ajax.test-danit.com/api/cards/${id}`,{
-                method: 'DELETE',
-                headers:{
-                    'Authorization': ` Bearer ${localStorage.tokenData.split('\"')[1]}`
-                }
-            });
+        const response = await fetch(`https://ajax.test-danit.com/api/cards/${id}`,{
+            method: 'DELETE',
+            headers:{
+                'Authorization': ` Bearer ${localStorage.tokenData.split('\"')[1]}`
+            }
+        });
         const data = await response.text();
         if(!data.length) {
             return true;
         }
         return false;
     }
- }
+}
 
- const server = new Fetch();
- signIn.addEventListener('click', function (e) {
+const server = new Fetch();
+signIn.addEventListener('click', function (e) {
+    server.getToken(email.value, password.value)
+    console.log('+++')
      e.preventDefault();
- });
+});
