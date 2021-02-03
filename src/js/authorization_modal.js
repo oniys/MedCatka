1a09228b48789f3c9a2a63a943f9cf8b34f37310
mac <oniys@mail.com>
2021-02-03 09:24:54 +0200

    add corect gulp form

--- src/js/authorization_modal.js
+++ src/js/authorization_modal.js
@@ -1,22 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const   modalButtons = document.querySelector('.aut-modal'),
+    const   activeButtons = document.querySelectorAll('.aut-modal'),
             overlay = document.querySelector('.over'),
-            closeButtons = document.querySelector('.close');
+            closeButtons = document.querySelectorAll('.close');
 
-        modalButtons.addEventListener('click', function(e) {
+    activeButtons.forEach(function (butItem)
+    {
+        butItem.addEventListener('click', function (e) {
             e.preventDefault();
             const modalId = this.getAttribute('data-modal'),
                 modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
             modalElem.classList.add('active');
             overlay.classList.add('active');
         });
+    });
 
-        closeButtons.addEventListener('click', function(e) {
+    closeButtons.forEach(function (cloItem) {
+        cloItem.addEventListener('click', function(e) {
             const parentModal = this.closest('.modal');
             parentModal.classList.remove('active');
             overlay.classList.remove('active');
         });
-
+    });
 
     document.body.addEventListener('keyup', function (e) {
         const key = e.keyCode;
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,3 @@
-import start from './access';
-start();
-
 @@include('authorization_modal.js');
 @@include('search.js');
-
+@@include('fetch.js');
